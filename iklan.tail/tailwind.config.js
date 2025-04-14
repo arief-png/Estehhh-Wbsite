cc439416fae2539f7a36e6dade5c599fc51ef365
Ackerman <ariefalsulthan321@gmail.com>
2025-04-14 19:15:37 +0700

    Add files via upload

---
+++ iklan.tail/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          bebas: ['"Bebas Neue"', 'cursive'], // font alternatif bisa bebas kamu ganti
+          boldonse: ['Boldonse', 'sans-serif'],
+        }
+      }
+    }
+  }
+  
